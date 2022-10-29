7a463e1bc037e8cde13057e849547532e34cae2b
Karen Anchundia Rivera <karennahomy23@gmail.com>
2022-10-28 20:08:10 -0500

    creacion de  ejercicio 5

---
+++ PRIMER PARCIAL/SEMANA 5/ejercicio.js
@@ -0,0 +1,67 @@
+const cors = require("cors");
+const express  = require("express");
+
+const app =  express();
+const PUERTO =  3000;
+
+app.use(cors()).use(express.json())
+app.use('/public', express.static(__dirname+'/public') )
+
+let tutores = [];
+
+
+app.get('/', (req,res)=>{
+    res.status(200).send(
+        tutores
+    )
+})
+app.get('/:id', (req,res)=>{
+    const {id} =  req.params;
+    // req.params.identification
+    let result = tutores.filter(p => p.id === id);
+    if (result.length>0)
+    {
+        res.status(200).send(result[0]);
+    }
+    res.status(404).send({
+        "message":"No se puede encontrar el elemento con esa identificación!"
+    });
+})
+app.post('/', (req,res)=>{
+    const {body} = req;
+    // req.body.name
+    // req.body.address
+    tutores.push(body);
+    res.status(200).send({
+        message:"Dato insertado correctamente",
+        response: body
+    })
+})
+app.put('/', (req,res)=>{
+    const {id, nombre, apellido, identificacion, experticia} = req.body;
+    let tutor =  tutores.filter(p=> p.id === id)[0] || {}
+    if (tutor.length)
+    {
+        tutor.nombre = nombre;
+        tutor.apellido = apellido;
+        tutor.identificacion = identificacion;
+        tutor.experticia = experticia;
+    }
+    res.status(200).send(
+        {
+            message:"El dato fue modificado correctamente",
+            response: tutor
+        }
+    )
+})
+
+app.delete('/:id', (req,res)=>{
+    const {id} =  req.params;
+    tutores = tutores.filter(p => p.id !== id);
+    res.status(200).send({
+        response:"Se eliminó el tutor con éxito!"
+    })
+})
+app.listen(PUERTO, ()=>{
+    console.log(`Servidor corriendo, acceda a http://localhost:${PUERTO}`)
+})
