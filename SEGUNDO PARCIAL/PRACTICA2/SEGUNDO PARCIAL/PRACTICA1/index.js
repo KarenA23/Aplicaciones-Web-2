ed097b09997b7f6530497e70429e1434c03b0809
KarenA23 <karennahomy23@gmail.com>
2022-12-30 22:23:18 -0500

    Creacion de Microservicios Practica 2

---
+++ SEGUNDO PARCIAL/PRACTICA2/SEGUNDO PARCIAL/PRACTICA1/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const Server  = require('./server');
+
+const server = new Server();
+server.listen();
+
+
+
+
+
+
---
+++ SEGUNDO PARCIAL/PRACTICA2/SEGUNDO PARCIAL/PRACTICA1/models/tutorias.js
@@ -0,0 +1,32 @@
+const { model, Schema } = require('mongoose');
+
+const TutoriasSchema = Schema(
+    {
+        idtutorado:{
+            type: Number,
+            default:0
+        },
+        idtutor:{
+            type: Number,
+            default:0
+        },
+        asignatura:{
+            type: String,
+            required: [ true, 'La asignatura es obligatoria'],
+        },
+        ndehoras:{
+            type: Number,
+            default:0
+        },
+        fecha:{
+            type: String,
+            required: [ true, 'La fecha es obligatoria'],
+        },
+        hora:{
+            type: String,
+            required: [ true, ' La hora es boligatoria'],
+        },
+    }
+);
+
+module.exports = model('Tutorias', TutoriasSchema );
